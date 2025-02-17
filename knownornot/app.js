4b6bb98745408600f64e0e2ee9ee9cb0b654a808
kelyan danis <kelyan.danis@ynov.com>
2025-02-17 15:15:20 +0100

    ✨ i add interface for user sign in an sign up, but i dosen't have add redirection or real sign up because the project dosen't have bdd for now ✨

--- knownornot/app.js
+++ knownornot/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var signinRouter = require("./routes/signin");
+var signupRouter = require("./routes/signup");
 
 var app = express();
 
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', signinRouter);
+app.use('/signup', signupRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
