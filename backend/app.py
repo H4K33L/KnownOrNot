2d0402ebaac8959753eb1a11eef3c1ada5f057d2
typhnz <tyvdphsn@gmail.com>
2025-02-25 16:27:20 +0100

    front end

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)  # Initialize Flask app
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
-
+    return "hello, this is our flask server""
+    
 if __name__ == '__main__':
     app.run(debug=True)  # Start the server
